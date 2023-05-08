453ca46bfe4be66c77ab9fad57cc5201c250ec5a
Tariq Hussain <tariqhussainchang363@gmail.com>
2023-05-08 19:03:23 +0500

    Added Create Blog and login, logout buttons to navbar

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -4,19 +4,40 @@ import "./Navbar.css";
 
 function Navbar() {
     return (
-        <div>
-            <nav className="nav__container">
-                <ul>
-                    <li>
-                        <Link to="/" className="link">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/blogs" className="link">Blogs</Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    )
+      <div>
+        <nav className="nav__container">
+          <button className="orange__btn">
+            <Link to="/create_form" className="create__btn">
+              Create Blog
+            </Link>
+          </button>
+          <ul>
+            <li>
+              <Link to="/" className="link">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/blogs" className="link">
+                Blogs
+              </Link>
+            </li>
+          </ul>
+          <div className="auth__container">
+            <button className="orange__btn">
+              <Link to="/create_blog" className="create__btn">
+                Login
+              </Link>
+            </button>
+            <button className="orange__btn">
+              <Link to="/create_blog" className="create__btn">
+                Sign Up
+              </Link>
+            </button>
+          </div>
+        </nav>
+      </div>
+    );
 }
 
 export default Navbar
